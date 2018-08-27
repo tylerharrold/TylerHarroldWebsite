25c07decec5ff731aec2311f40fb0123367d09a6
tylerharrold <tylerharrold@gmail.com>
2018-08-27 19:27:00 +0000

    Added under construction page

--- __init__.py
+++ __init__.py
@@ -10,5 +10,9 @@ def hello():
 def projects():
 	return render_template('projects_page.html')
 
+@app.route("/construction")
+def construction():
+	return render_template('under_construction.html')
+
 if __name__ == "__main__":
 	app.run()
